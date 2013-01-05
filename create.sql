2b81cd32abc4e43a4f3f6c76d8112f2fc0d3f52d
Droogans <droogans@gmail.com>
2013-01-05 13:14:08 -0500

    needs more work...

--- create.sql
+++ create.sql
@@ -1,41 +1,35 @@
 
 -- All tables used for PLC monitoring database
 
+USE USERNAME$tutorial;
+
 DELIMITER $$
 
 CREATE PROCEDURE createDB()
 BEGIN
-
-IF 
-	SELECT Versions.id = 1 THEN
 	CREATE TABLE IF NOT EXISTS PLCs(
-		id 				INT AUTO_INCREMENT,
-		serialNumber 	VARCHAR(32) NOT NULL,
-		friendlyName 	VARCHAR(32),
-		locName			VARCHAR(64) 
-		
+		id INT NOT NULL AUTO_INCREMENT,
+		serialNumber VARCHAR(32) NOT NULL,
+		friendlyName VARCHAR(32),
+		locName VARCHAR(64), 		
 		PRIMARY KEY(id),
 		UNIQUE INDEX(serialNumber),
 		UNIQUE INDEX(friendlyName),
-		
-		FOREIGN KEY(locName) REFERENCES
+		FOREIGN KEY(locName) REFERENCES 
 			Locations(locName) ON DELETE RESTRICT
 	)$$
 
 	CREATE TABLE IF NOT EXISTS Locations(
-		id				INT AUTO_INCREMENT,
-		name			VARCHAR(64) 
-		
+		id INT AUTO_INCREMENT,
+		name VARCHAR(64),
 		PRIMARY KEY(id)
 	)$$
 
 	CREATE TABLE IF NOT EXISTS Installations(
-		plcID			INT NOT NULL,
-		engID			INT NOT NULL,
-		
+		plcID INT NOT NULL,
+		engID INT NOT NULL,	
 		PRIMARY KEY (plcID, engID),
 		UNIQUE INDEX(plcID, engID),
-		
 		FOREIGN KEY (plcID) REFERENCES
 			(PLCs.id) ON DELETE RESTRICT,
 		FOREIGN KEY (engID) REFERENCES
@@ -43,21 +37,14 @@ IF
 	)$$
 
 	CREATE TABLE IF NOT EXISTS Engineers(
-		id				INT AUTO_INCREMENT,
-		fName			VARCHAR(64) NOT NULL,
-		lName			VARCHAR(64) NOT NULL,
-		
+		id INT AUTO_INCREMENT,
+		fName VARCHAR(64) NOT NULL,
+		lName VARCHAR(64) NOT NULL,
 		PRIMARY KEY(id)
 	)$$
-
-END IF$$
 
 END$$
 
 DELIMITER ;
-
-CREATE TABLE IF NOT EXISTS Versions(
-	id				INT PRIMARY KEY
-);
 
 CALL createDB();
